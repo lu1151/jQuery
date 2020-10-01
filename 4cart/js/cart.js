684c45b9249f72919c7a507b9b330b4801d20c9d
lu1151 <luxi.li@outlook.com>
2020-09-30 22:15:44 -0400

    Third commit

--- 4cart/js/cart.js
+++ 4cart/js/cart.js
@@ -20,6 +20,7 @@ $(function () {
       .parents(".p-num")
       .siblings(".p-sum")
       .html("￥" + price);
+    getSum();
   });
   $(".decrement").click(function () {
     var n = $(this).siblings(".itxt").val();
@@ -35,18 +36,34 @@ $(function () {
       .parents(".p-num")
       .siblings(".p-sum")
       .html("￥" + price);
+    getSum();
   });
-  $(".itxt").change(function(){
+  $(".itxt").change(function () {
     var n = $(this).val();
-    if (n<1){
+    if (n < 1) {
       $(this).val("1");
-    } else{
-    var p = $(this).parents(".p-num").siblings(".p-price").html();
-    p = p.substr(1);
-    var price = (p * n).toFixed(2);
-    $(this)
-      .parents(".p-num")
-      .siblings(".p-sum")
-      .html("￥" + price);
-  }})
+    } else {
+      var p = $(this).parents(".p-num").siblings(".p-price").html();
+      p = p.substr(1);
+      var price = (p * n).toFixed(2);
+      $(this)
+        .parents(".p-num")
+        .siblings(".p-sum")
+        .html("￥" + price);
+    }
+    getSum();
+  });
+  getSum();
+  function getSum() {
+    var count = 0;
+    var sum = 0;
+    $(".itxt").each(function (i, ele) {
+      count += parseInt($(ele).val());
+      $(".amount-sum em").text(count);
+    });
+    $(".p-sum").each(function (i, ele) {
+      sum += parseFloat($(ele).text().substr(1));
+      $(".price-sum em").text("￥" + sum.toFixed(2));
+    });
+  }
 });
